a7a42f564c7a3d1dca886bc06367a8bd9e3bfc3b
Kirill Zhuchkov <643378+r0busta@users.noreply.github.com>
2022-03-14 10:03:01 +0100

    Init clerk client on demand

--- src/lib/clerk.ts
+++ src/lib/clerk.ts
@@ -1,4 +1,9 @@
+import Clerk from "@clerk/clerk-sdk-node/instance"
 import { NextApiRequest } from "next/types"
+
+export function newClient() {
+    return new Clerk({ apiKey: process.env.CLERK_API_KEY || "" })
+}
 
 export function getClerkSessionToken(req: NextApiRequest) {
     return req.cookies["__session"]
--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,6 +1,6 @@
-import clerk from "@clerk/clerk-sdk-node"
 import ioredis from "ioredis"
 import * as redis from "./redis"
+import * as clerk from "./clerk"
 import { parseJwt } from "./token"
 import getShopify from "./shopify"
 
@@ -283,7 +283,7 @@ async function getUserId(clerkSessionToken: string): Promise<string | undefined>
     try {
         const sessionId = parseJwt(clerkSessionToken)?.sid
 
-        const clerkSession = await clerk.sessions.getSession(sessionId)
+        const clerkSession = await clerk.newClient().sessions.getSession(sessionId)
         if (clerkSession.userId) {
             userId = clerkSession.userId
         }
