64660de76cd81a9a06f5b30fa04f35b9bfa4e814
Kirill Zhuchkov <643378+r0busta@users.noreply.github.com>
2023-11-29 16:24:31 +0100

    adapt to the Clerk's changes

--- src/lib/clerk.ts
+++ src/lib/clerk.ts
@@ -1,7 +1,5 @@
 import { NextApiRequest } from "next/types"
-import { setClerkApiKey, sessions as clerkSessions } from "@clerk/clerk-sdk-node"
-
-setClerkApiKey(process.env.CLERK_API_KEY || "")
+import { sessions as clerkSessions } from "@clerk/clerk-sdk-node"
 
 export function sessionsApi() {
     return clerkSessions
